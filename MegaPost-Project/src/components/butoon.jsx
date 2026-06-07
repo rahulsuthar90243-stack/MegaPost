4ec7d2d459cf7fd5c6dbd7cdb657f820e0bc6188
Rahul <rahulsuthar90243@gmail.com>
2026-06-07 23:16:44 +0530

    button component with customizable styles and props

---
+++ MegaPost-Project/src/components/butoon.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+function button({
+children,
+type = "button",
+bgColor = "bg-blue-500",
+textColor = "text-white",
+ClassName = '',
+...props
+}){
+    return (
+        <button className={'px-4 py-2 rounded ${bgColor} ${textColor} ${ClassName}'} {...props}>
+            {children}
+        </button>
+    )
+}
