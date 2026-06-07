c085a54cd565edc9ea322044c85f743b2ad521c5
Rahul <rahulsuthar90243@gmail.com>
2026-06-07 23:13:59 +0530

    Implement Header component with authentication navigation

--- MegaPost-Project/src/components/Header/Header.jsx
+++ MegaPost-Project/src/components/Header/Header.jsx
@@ -1,7 +1,63 @@
-export const Header = () => {
-  return (
-    <div>Header</div>
-  )
+import React from "react";
+import { Container, Logo, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function Header() {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
+  return 
+  <header className='p-4 bg-gray-800 text-white'>
+    <Container>
+      <nav className='flex items-center justify-between'>
+        <div className='flex items-center space-x-4'>
+          <Logo width="120px" />
+        </div>
+        <ul className='flex ml-auto'>
+          {navItems.map((item) => item.active ? (
+            <li key={item.name}>
+              <button onClick={() => navigate(item.slug)} className='px-3 py-2 text-sm font-medium hover:bg-gray-700 rounded'>
+                {item.name}</button>
+            </li>
+          ) : null)}
+          {authStatus && (
+            <li>
+              <LogoutBtn />
+            </li>
+          )}
+        </ul>
+      </nav>
+    </Container>
+  </header>
 }
 
 export default Header;
