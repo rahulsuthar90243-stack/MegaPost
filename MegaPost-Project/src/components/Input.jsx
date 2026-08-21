11519b982f0aaef1b122a7a0ef0e577d4ea16363
Rahul <rahulsuthar90243@gmail.com>
2026-08-21 15:18:33 +0530

    feat: add input component for form fields and inputs

---
+++ MegaPost-Project/src/components/Input.jsx
@@ -0,0 +1,32 @@
+import React, { use } from 'react'
+import { useId } from 'react'
+
+const Input = React.forwardRef(function Input({
+    label,
+    type = 'text',
+    className = "",
+    ...props
+}, ref){
+   const id = useId()
+   return (
+    <div className='w-full'>
+        {label && <label 
+        className="block mb-1 text-sm font-medium text-gray-900 dark:text-white" htmlFor=
+        {id}>{label}
+        </label>
+        }
+        <input
+        type={type}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full
+            ${className}`}
+            ref={ref}
+            {...props}
+            id={id}
+        />
+    </div>
+   )
+})
+
+
+export default Input
+
